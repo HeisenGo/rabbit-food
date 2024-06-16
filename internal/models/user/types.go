1faa9d73279352b9d8793012e07d85e4ae7cb656
kiarashfz <kiarashfz.fazli@gmail.com>
2024-06-16 17:46:00 +0330

    feat: user struct and interface

---
+++ internal/models/user/types.go
@@ -0,0 +1,31 @@
+package user
+
+import (
+	"rabbit-food/pkg/adapters/storage/entities"
+	"time"
+)
+
+type Repo interface {
+	Create(user *User) (*entities.User, error)
+	//Create(ctx context.Context, user *User) error
+	//GetByID(ctx context.Context, id uint) (*User, error)
+	//GetByEmail(ctx context.Context, email string) (*User, error)
+}
+
+type User struct {
+	ID        uint
+	Phone     string
+	Email     string
+	FirstName string
+	LastName  string
+	Password  string
+	BirthDate time.Time
+}
+
+func NewUser(phone, email, password string) *User {
+	return &User{
+		Phone:    phone,
+		Email:    email,
+		Password: password,
+	}
+}
