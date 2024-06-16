b89790d47f4dfa1b614f436056207c411f0c4223
kiarashfz <kiarashfz.fazli@gmail.com>
2024-06-16 17:44:12 +0330

    feat: messages structs

--- internal/protocol/message.go
+++ internal/protocol/message.go
@@ -1,1 +1,13 @@
 package protocol
+
+type RegisterRequest struct {
+	Phone    string
+	Email    string
+	Password string
+}
+
+type RegisterResponse struct {
+	Success bool
+	Message string
+	UserID  uint
+}
