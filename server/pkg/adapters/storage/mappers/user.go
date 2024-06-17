afc9d55a5f02e4612f1c50f9938a2f08c242dccd
kiarashfz <kiarashfz.fazli@gmail.com>
2024-06-18 01:03:44 +0330

    feat: mapper of domain and entities

---
+++ server/pkg/adapters/storage/mappers/user.go
@@ -0,0 +1,24 @@
+package mappers
+
+import (
+	"server/internal/models/user"
+	"server/pkg/adapters/storage/entities"
+)
+
+func UserEntityToDomain(entity *entities.User) *user.User {
+	return &user.User{
+		ID:        entity.ID,
+		FirstName: entity.FirstName,
+		LastName:  entity.LastName,
+		Email:     entity.Email,
+		Password:  entity.Password,
+	}
+}
+
+func UserDomainToEntity(domainUser *user.User) *entities.User {
+	return &entities.User{
+		Phone:    domainUser.Phone,
+		Email:    domainUser.Email,
+		Password: domainUser.Password,
+	}
+}
