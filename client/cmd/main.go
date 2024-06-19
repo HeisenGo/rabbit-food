71bbe813620e634f9ac5c12a0f48fb5f37f5771d
kiarashfz <kiarashfz.fazli@gmail.com>
2024-06-19 20:43:39 +0330

    feat: parent directory removed and main functionalities added

---
+++ client/cmd/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"client/config"
+	"client/menus"
+	"client/utils"
+	"flag"
+	"os"
+)
+
+func main() {
+	utils.WelcomeSayer()
+	defer utils.ByePrinter()
+	cfg := readConfig()
+	scanner := bufio.NewScanner(os.Stdin)
+
+	mainMenu := menus.CreateMainMenu(cfg.GetService())
+	mainMenu.Execute(scanner)
+}
+
+var envFilePath = flag.String("envpath", "", ".env file path")
+
+func readConfig() config.IConfig {
+	flag.Parse()
+	return config.ReadConfig(*envFilePath)
+}
