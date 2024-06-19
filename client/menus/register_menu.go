02b07556fb988445d6a867b425807a73eb7197be
kiarashfz <kiarashfz.fazli@gmail.com>
2024-06-19 20:48:32 +0330

    feat: RegisterMenuItem implementation

---
+++ client/menus/register_menu.go
@@ -0,0 +1,49 @@
+package menus
+
+import (
+	"bufio"
+	"client/commands"
+	"client/constants"
+	"client/models"
+	"client/utils"
+	"fmt"
+	"time"
+)
+
+type RegisterMenuItem struct {
+	Name     string
+	Command  *commands.RegisterCommand
+	PostMenu MenuComponent
+}
+
+func NewRegisterMenuItem(name string, command *commands.RegisterCommand, postMenu MenuComponent) *RegisterMenuItem {
+	return &RegisterMenuItem{
+		Name:     name,
+		Command:  command,
+		PostMenu: postMenu,
+	}
+}
+
+func (mi *RegisterMenuItem) Display() {
+	fmt.Println(mi.Name)
+}
+
+func (mi *RegisterMenuItem) Execute(scanner *bufio.Scanner) {
+	defer time.Sleep(time.Second)
+	utils.ClearScreen()
+	utils.ColoredPrint(constants.Blue, fmt.Sprintf("[------------ %s ------------] \n", mi.Name))
+	var user models.User
+	user.Phone = utils.ReadInput(scanner, "Phone: ")
+	user.Email = utils.ReadInput(scanner, "Email: ")
+	user.Password = utils.ReadInput(scanner, "Password: ")
+	err := mi.Command.Execute(&user)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// TODO: mi.state = ProfileMngmnt state
+	if mi.PostMenu != nil {
+		mi.PostMenu.Execute(scanner)
+		return
+	}
+}
