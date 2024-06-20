db39be98f60c82b7b4f4d5ef7d8d6cdb37e60260
kiarashfz <kiarashfz.fazli@gmail.com>
2024-06-21 01:55:50 +0330

    fix: duplicate phone and email handled

--- server/pkg/adapters/storage/user.go
+++ server/pkg/adapters/storage/user.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"gorm.io/gorm"
+	"server/internal/errors/users"
 	"server/internal/models/user"
 	"server/pkg/adapters/storage/entities"
 	"server/pkg/adapters/storage/mappers"
@@ -17,6 +18,9 @@ func (r *userRepo) Create(ctx context.Context, user *user.User) (*user.User, err
 	newUser := mappers.UserDomainToEntity(user)
 	err := r.db.Create(&newUser).Error
 	if err != nil {
+		if errors.Is(err, gorm.ErrDuplicatedKey) {
+			return nil, users.ErrUserExists
+		}
 		return nil, err
 	}
 	createdUser := mappers.UserEntityToDomain(newUser)
@@ -24,27 +28,27 @@ func (r *userRepo) Create(ctx context.Context, user *user.User) (*user.User, err
 }
 
 func (r *userRepo) GetByPhone(ctx context.Context, phone string) (*user.User, error) {
-	var user entities.User
-	err := r.db.WithContext(ctx).Model(&entities.User{}).Where("Phone = ?", phone).First(&user).Error
+	var userEntity entities.User
+	err := r.db.WithContext(ctx).Model(&entities.User{}).Where("Phone = ?", phone).First(&userEntity).Error
 	if err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return nil, nil
+			return nil, users.ErrUserNotFound
 		}
 		return nil, err
 	}
-	return mappers.UserEntityToDomain(&user), nil
+	return mappers.UserEntityToDomain(&userEntity), nil
 }
 
 func (r *userRepo) GetByEmail(ctx context.Context, email string) (*user.User, error) {
-	var user entities.User
-	err := r.db.WithContext(ctx).Model(&entities.User{}).Where("Email = ?", email).First(&user).Error
+	var userEntity entities.User
+	err := r.db.WithContext(ctx).Model(&entities.User{}).Where("Email = ?", email).First(&userEntity).Error
 	if err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return nil, nil
+			return nil, users.ErrUserNotFound
 		}
 		return nil, err
 	}
-	return mappers.UserEntityToDomain(&user), nil
+	return mappers.UserEntityToDomain(&userEntity), nil
 }
 
 func NewUserRepo(db *gorm.DB) user.Repo {
