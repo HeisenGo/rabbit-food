368f526cae993c9466bbfcac13bf903c64492498
kiarashfz <kiarashfz.fazli@gmail.com>
2024-06-20 18:28:25 +0330

    feat: Login & Logout methods added to Service interface

--- client/services/service.go
+++ client/services/service.go
@@ -4,5 +4,7 @@ import "client/models"
 
 type Service interface {
 	Register(*models.User) error
+	Login(req *models.LoginUserReq) (*models.User, error)
+	Logout(req *models.LogoutUserReq) error
 	GetWallet(req *models.GetWalletReq) (*models.Wallet, error)
 }
