609ac66f1993b618767062288d2fd4ede3c76c34
kiarashfz <kiarashfz.fazli@gmail.com>
2024-06-24 17:00:42 +0330

    feat: deposit and withdraw ops

--- server/internal/models/wallet/wallet/ops.go
+++ server/internal/models/wallet/wallet/ops.go
@@ -3,6 +3,7 @@ package wallet
 import (
 	"context"
 	"gorm.io/gorm"
+	creditCard "server/internal/models/wallet/credit_card"
 )
 
 type WalletOps struct {
@@ -19,4 +20,12 @@ func NewWalletOps(db *gorm.DB, repo Repo) *WalletOps {
 
 func (o *WalletOps) Create(ctx context.Context, wallet *Wallet) (*Wallet, error) {
 	return o.repo.Create(ctx, wallet)
+}
+
+func (o *WalletOps) Deposit(ctx context.Context, creditCard *creditCard.CreditCard, amount uint) (*Wallet, error) {
+	return o.repo.Deposit(ctx, creditCard, amount)
+}
+
+func (o *WalletOps) Withdraw(ctx context.Context, creditCard *creditCard.CreditCard, amount uint) (*Wallet, error) {
+	return o.repo.Withdraw(ctx, creditCard, amount)
 }
