857022ecfa8518bbc0a409072e1c05c00961c1ba
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2024-06-25 02:35:01 +0330

    Update message.go
    feat: Add UserProfile request and response types to protocol

--- server/internal/protocol/tcp/message.go
+++ server/internal/protocol/tcp/message.go
@@ -1,20 +1,45 @@
 package tcp
 
-import "server/internal/models/auth"
+import (
+	"server/internal/models/auth"
+	"server/internal/models/user"
+	"time"
+)
 
 type RegisterRequest struct {
-	Phone    string  `json:"phone"`
-	Email    *string `json:"email"`
-	Password string  `json:"password"`
+	Phone     string  `json:"phone"`
+	Email     *string `json:"email"`
+	Password  string  `json:"password"`
+	FirstName string  `json:"first_name"`
+	LastName  string  `json:"last_name"`
 }
+
 type RegisterResponse struct {
 	Message string
 	Token   *auth.Token
 }
+
+type UserRequest struct {
+	ID        uint      `json:"id"`
+	Phone     string    `json:"phone"`
+	Email     *string   `json:"email"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Password  string    `json:"password"`
+	BirthDate time.Time `json:"birth_date"`
+	Address   string    `json:"address"`
+}
+
+type UserResponse struct {
+	Message string
+	User    *user.User
+}
+
 type LoginRequest struct {
 	PhoneOrEmail string `json:"phone_or_email"`
 	Password     string `json:"password"`
 }
+
 type LoginResponse struct {
 	Message   string
 	AuthToken *auth.Token
