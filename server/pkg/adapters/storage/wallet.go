1fc057c9b0e16dc6edb2897a128ee724b5a8ce7c
kiarashfz <kiarashfz.fazli@gmail.com>
2024-06-24 16:58:42 +0330

    feat: deposit and withdraw wallet repo methods

--- server/pkg/adapters/storage/wallet.go
+++ server/pkg/adapters/storage/wallet.go
@@ -2,9 +2,13 @@ package storage
 
 import (
 	"context"
+	"errors"
 	"gorm.io/gorm"
+	creditCard "server/internal/models/wallet/credit_card"
 	"server/internal/models/wallet/wallet"
+	"server/pkg/adapters/storage/entities"
 	"server/pkg/adapters/storage/mappers"
+	"server/pkg/utils"
 )
 
 type walletRepo struct {
@@ -23,5 +27,101 @@ func (r *walletRepo) Create(ctx context.Context, wallet *wallet.Wallet) (*wallet
 		return nil, err
 	}
 	createdWallet := mappers.WalletEntityToDomain(newWallet)
+	return createdWallet, nil
+}
+
+func (r *walletRepo) Deposit(ctx context.Context, card *creditCard.CreditCard, amount uint) (*wallet.Wallet, error) {
+	var userWalletEntity *entities.Wallet
+	var cardEntity *entities.CreditCard
+
+	tx := r.db.Begin()
+	if tx.Error != nil {
+		return nil, tx.Error
+	}
+
+	defer func() {
+		if rv := recover(); rv != nil {
+			tx.Rollback()
+		}
+	}()
+
+	userID, err := utils.GetUserIDFromContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	if err = tx.Where("user_id = ?", userID).First(&userWalletEntity).Error; err != nil {
+		tx.Rollback()
+		return nil, err
+	}
+
+	// Check if the credit card exists and belongs to the user's wallet
+	if err = tx.Joins("JOIN wallet_credit_cards ON wallet_credit_cards.credit_card_id = credit_cards.id").
+		Joins("JOIN wallets ON wallets.id = wallet_credit_cards.wallet_id").
+		Where("credit_cards.number = ? AND wallets.user_id = ?", card.Number, userID).
+		First(&cardEntity).Error; err != nil {
+		tx.Rollback()
+		return nil, err
+	}
+
+	// Increase the wallet balance
+	userWalletEntity.Balance += amount
+	if err = tx.Save(&userWalletEntity).Error; err != nil {
+		tx.Rollback()
+		return nil, err
+	}
+
+	if err = tx.Commit().Error; err != nil {
+		return nil, err
+	}
+	createdWallet := mappers.WalletEntityToDomain(userWalletEntity)
+	return createdWallet, nil
+}
+
+func (r *walletRepo) Withdraw(ctx context.Context, card *creditCard.CreditCard, amount uint) (*wallet.Wallet, error) {
+	var userWalletEntity *entities.Wallet
+	var cardEntity *entities.CreditCard
+
+	tx := r.db.Begin()
+	if tx.Error != nil {
+		return nil, tx.Error
+	}
+
+	defer func() {
+		if rv := recover(); rv != nil {
+			tx.Rollback()
+		}
+	}()
+
+	userID, err := utils.GetUserIDFromContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	if err = tx.Where("user_id = ?", userID).First(&userWalletEntity).Error; err != nil {
+		tx.Rollback()
+		return nil, err
+	}
+
+	// Check if the credit card exists and belongs to the user's wallet
+	if err = tx.Joins("JOIN wallet_credit_cards ON wallet_credit_cards.credit_card_id = credit_cards.id").
+		Joins("JOIN wallets ON wallets.id = wallet_credit_cards.wallet_id").
+		Where("credit_cards.number = ? AND wallets.user_id = ?", card.Number, userID).
+		First(&cardEntity).Error; err != nil {
+		tx.Rollback()
+		return nil, err
+	}
+
+	if userWalletEntity.Balance < amount {
+		return nil, errors.New("not enough balance to withdraw")
+	}
+	userWalletEntity.Balance -= amount
+	if err = tx.Save(&userWalletEntity).Error; err != nil {
+		tx.Rollback()
+		return nil, err
+	}
+
+	if err = tx.Commit().Error; err != nil {
+		return nil, err
+	}
+	createdWallet := mappers.WalletEntityToDomain(userWalletEntity)
 	return createdWallet, nil
 }
