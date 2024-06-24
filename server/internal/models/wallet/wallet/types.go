75f9e74260aab29a730145a9a574b6c73989cd7d
kiarashfz <kiarashfz.fazli@gmail.com>
2024-06-24 16:59:45 +0330

    feat: deposit and withdraw methods added to wallet repo interface

--- server/internal/models/wallet/wallet/types.go
+++ server/internal/models/wallet/wallet/types.go
@@ -2,10 +2,13 @@ package wallet
 
 import (
 	"context"
+	creditCard "server/internal/models/wallet/credit_card"
 )
 
 type Repo interface {
 	Create(ctx context.Context, user *Wallet) (*Wallet, error)
+	Deposit(ctx context.Context, creditCard *creditCard.CreditCard, amount uint) (*Wallet, error)
+	Withdraw(ctx context.Context, creditCard *creditCard.CreditCard, amount uint) (*Wallet, error)
 }
 
 type Wallet struct {
