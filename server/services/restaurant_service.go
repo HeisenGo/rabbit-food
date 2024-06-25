38945c8e2bc937ecf3c54977420fd04058fc3817
Neda Hn <z.hosseini.7@gmail.com>
2024-06-25 18:49:16 +0330

    fix: a wrong commit

---
+++ server/services/restaurant_service.go
@@ -0,0 +1,32 @@
+package services
+
+import (
+	"context"
+	"server/internal/models/restaurant/restaurant"
+)
+
+type RestaurantService struct {
+	restauarntOps *restaurant.Ops
+}
+
+func NewRestaurantService(restauarntOps *restaurant.Ops) *RestaurantService {
+	return &RestaurantService{
+		restauarntOps: restauarntOps,
+	}
+}
+
+func (s *RestaurantService) CreateResturantForOwner(ctx context.Context, restaurant *restaurant.Restaurant) (*restaurant.Restaurant, error) {
+	createdWallet, err := s.restauarntOps.Create(ctx, restaurant)
+	if err != nil {
+		return nil, err
+	}
+	return createdWallet, nil
+}
+
+func (s *RestaurantService) GetRestaurantByID(ctx context.Context, restaurantID uint) (*restaurant.Restaurant, error) {
+	createdWallet, err := s.restauarntOps.Create(ctx, restaurant)
+	if err != nil {
+		return nil, err
+	}
+	return createdWallet, nil
+}
