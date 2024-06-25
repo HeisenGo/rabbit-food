0caaf689a7c3a928a615d9b9cccd162d4b8618f0
Neda Hn <z.hosseini.7@gmail.com>
2024-06-26 03:20:15 +0330

    feat: ops and types of restaurant updated accordig to queries

--- server/internal/models/restaurant/restaurant/ops.go
+++ server/internal/models/restaurant/restaurant/ops.go
@@ -2,6 +2,7 @@ package restaurant
 
 import (
 	"context"
+	"server/internal/models/restaurant/motor"
 	"server/internal/models/user"
 
 	"gorm.io/gorm"
@@ -33,4 +34,37 @@ func (o *Ops) GetByID(ctx context.Context, restaurantID uint) (*Restaurant, erro
 
 func (o *Ops) AssignOperatorToRestarant(ctx context.Context, operator *user.User, restaurant Restaurant) (*user.User, error) {
 	return o.repo.AssignOperatorToRestarant(ctx, operator, restaurant)
+}
+
+func (o *Ops) RemoveOperatorFromRestarant(ctx context.Context, operatorID uint, restaurantID uint) error {
+	return o.repo.RemoveOperatorFromRestarant(ctx, operatorID, restaurantID)
+}
+
+func (o *Ops) AddMotor(ctx context.Context, motor *motor.Motor, restaurantID uint) (*motor.Motor, error) {
+	return o.repo.AddMotor(ctx, motor, restaurantID)
+}
+
+func (o *Ops) RemoveMotor(ctx context.Context, motorID uint) error {
+	return o.repo.RemoveMotor(ctx, motorID)
+}
+
+func (o *Ops) WithdrawRestaurant(ctx context.Context, newOwnerID uint, restaurantID uint) error {
+	return o.repo.WithdrawRestaurant(ctx, newOwnerID, restaurantID)
+}
+
+func (o *Ops) GetAllMotors(ctx context.Context, restaurantID uint) ([]*motor.Motor, error) {
+	return o.repo.GetAllMotors(ctx, restaurantID)
+}
+func (o *Ops) GetAllOperators(ctx context.Context, restaurantID uint) ([]*user.User, error) {
+	return o.repo.GetAllOperators(ctx, restaurantID)
+}
+func (o *Ops) DoeseThisHaveARoleInRestaurant(ctx context.Context, restaurantID uint) (bool, error) {
+	return o.repo.DoeseThisHaveARoleInRestaurant(ctx, restaurantID)
+}
+func (o *Ops) GetOwnerInfo(ctx context.Context, restaurantID uint) (*user.User, error) {
+	return o.repo.GetOwnerInfo(ctx, restaurantID)
+}
+func (o *Ops) GetRestarantInfo(ctx context.Context, restaurantID uint) (*Restaurant,
+	*user.User, []*user.User, []*motor.Motor, error) {
+	return o.repo.GetRestarantInfo(ctx, restaurantID)
 }
--- server/internal/models/restaurant/restaurant/types.go
+++ server/internal/models/restaurant/restaurant/types.go
@@ -2,6 +2,7 @@ package restaurant
 
 import (
 	"context"
+	"server/internal/models/restaurant/motor"
 	"server/internal/models/user"
 )
 
@@ -22,6 +23,15 @@ type Repo interface {
 	CheckMatchedRestaurantsOwnerIdAndClaimedID(ctx context.Context, restaurantID uint) (bool, error)
 	GetByID(ctx context.Context, restaurantID uint) (*Restaurant, error)
 	AssignOperatorToRestarant(ctx context.Context, operator *user.User, restaurant Restaurant) (*user.User, error)
+	RemoveOperatorFromRestarant(ctx context.Context, operatorID uint, restaurantID uint) error
+	WithdrawRestaurant(ctx context.Context, newOwnerID uint, restaurantID uint) error
+	AddMotor(ctx context.Context, motor *motor.Motor, restaurantID uint) (*motor.Motor, error)
+	RemoveMotor(ctx context.Context, motorID uint) error
+	GetAllMotors(ctx context.Context, restaurantID uint) ([]*motor.Motor, error)
+	GetAllOperators(ctx context.Context, restaurantID uint) ([]*user.User, error)
+	DoeseThisHaveARoleInRestaurant(ctx context.Context, restaurantID uint) (bool, error)
+	GetOwnerInfo(ctx context.Context, restaurantID uint) (*user.User, error) 
+	GetRestarantInfo(ctx context.Context, restaurantID uint) (*Restaurant, *user.User, []*user.User, []*motor.Motor, error) 
 }
 
 func NewRestaurant(name string, phone string, city string, address string, coordiantes string) *Restaurant {
