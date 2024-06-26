6c351a57234deb1471f049d6e0894f97ea442b0a
Neda Hn <z.hosseini.7@gmail.com>
2024-06-26 03:37:43 +0330

    feat: services for restaurant added according to ops and types

--- server/services/restaurant_service.go
+++ server/services/restaurant_service.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"context"
+	"server/internal/models/restaurant/motor"
 	"server/internal/models/restaurant/restaurant"
 	"server/internal/models/user"
 )
@@ -46,4 +47,73 @@ func (s *RestaurantService) AssignOperatorToRestarant(ctx context.Context, opera
 		return nil, err
 	}
 	return user, nil
+}
+
+func (s *RestaurantService) RemoveOperatorFromRestarant(ctx context.Context, operatorID uint, restaurantID uint) error {
+	err := s.restauarntOps.RemoveOperatorFromRestarant(ctx, operatorID, restaurantID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *RestaurantService) AddMotor(ctx context.Context, motor *motor.Motor, restaurantID uint) (*motor.Motor, error) {
+	motor, err := s.restauarntOps.AddMotor(ctx, motor, restaurantID)
+	if err != nil {
+		return nil, err
+	}
+	return motor, nil
+}
+
+func (s *RestaurantService) RemoveMotor(ctx context.Context, motorID uint) error {
+	err := s.restauarntOps.RemoveMotor(ctx, motorID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *RestaurantService) WithdrawRestaurant(ctx context.Context, newOwnerID uint, restaurantID uint) error {
+	err := s.restauarntOps.WithdrawRestaurant(ctx, newOwnerID, restaurantID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *RestaurantService) GetAllMotors(ctx context.Context, restaurantID uint) ([]*motor.Motor, error) {
+	motors, err := s.restauarntOps.GetAllMotors(ctx, restaurantID)
+	if err != nil {
+		return nil, err
+	}
+	return motors, nil
+}
+
+func (s *RestaurantService) GetAllOperators(ctx context.Context, restaurantID uint) ([]*user.User, error) {
+	oprators, err := s.restauarntOps.GetAllOperators(ctx, restaurantID)
+	if err != nil {
+		return nil, err
+	}
+	return oprators, nil
+}
+
+func (s *RestaurantService) DoeseThisHaveARoleInRestaurant(ctx context.Context, restaurantID uint) (bool, error) {
+	yes, err := s.restauarntOps.DoeseThisHaveARoleInRestaurant(ctx, restaurantID)
+	if err != nil {
+		return false, err
+	}
+	return yes, nil
+}
+
+func (s *RestaurantService) GetOwnerInfo(ctx context.Context, restaurantID uint) (*user.User, error) {
+	owner, err := s.restauarntOps.GetOwnerInfo(ctx, restaurantID)
+	if err != nil {
+		return nil, err
+	}
+	return owner, nil
+}
+
+func (s *RestaurantService) GetRestarantInfo(ctx context.Context, restaurantID uint) (*restaurant.Restaurant,
+	*user.User, []*user.User, []*motor.Motor, error) {
+		return s.restauarntOps.GetRestarantInfo(ctx, restaurantID)
 }
