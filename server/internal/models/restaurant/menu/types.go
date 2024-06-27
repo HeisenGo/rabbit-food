277f7720b8ecf42dcdd1210dfc0db7bef0d6a50e
kiarashfz <kiarashfz.fazli@gmail.com>
2024-06-27 05:08:01 +0330

    feat: domain models

---
+++ server/internal/models/restaurant/menu/types.go
@@ -0,0 +1,38 @@
+package menu
+
+import (
+	"context"
+	"server/internal/models/restaurant/restaurant"
+)
+
+type Menu struct {
+	ID           uint
+	Name         string
+	RestaurantID uint
+}
+
+type MenuItem struct {
+	ID              uint
+	Name            string
+	Price           uint
+	PreparationTime uint
+	MenuID          uint
+}
+
+func NewMenuItem(name string, price uint, preparationTime uint, menuID uint) *MenuItem {
+	return &MenuItem{Name: name, Price: price, PreparationTime: preparationTime, MenuID: menuID}
+}
+
+type Repo interface {
+	CreateMenu(ctx context.Context, menu *Menu) (*Menu, error)
+	GetAllRestaurantMenus(ctx context.Context, restaurant *restaurant.Restaurant) ([]*Menu, error)
+	AddMenuItemToMenu(ctx context.Context, menu *MenuItem) (*MenuItem, error)
+}
+
+func NewMenu(name string, restaurantID uint) *Menu {
+	menu := &Menu{
+		Name:         name,
+		RestaurantID: restaurantID,
+	}
+	return menu
+}
