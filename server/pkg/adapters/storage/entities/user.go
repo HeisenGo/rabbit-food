2a6de41ceb7021e22101e0f3aeec664604ed81dd
kiarashfz <kiarashfz.fazli@gmail.com>
2024-06-27 05:06:30 +0330

    feat: restaurants added

--- server/pkg/adapters/storage/entities/user.go
+++ server/pkg/adapters/storage/entities/user.go
@@ -7,12 +7,13 @@ import (
 
 type User struct {
 	gorm.Model
-	Phone     string  `gorm:"index:idx_phone,unique"`
-	Email     *string `gorm:"uniqueIndex:idx_email_not_null,where:email IS NOT NULL"`
-	FirstName string
-	LastName  string
-	Password  string
-	BirthDate time.Time
-	IsAdmin   bool    `gorm:"default:false"`
-	Wallet    *Wallet `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Phone       string  `gorm:"index:idx_phone,unique"`
+	Email       *string `gorm:"uniqueIndex:idx_email_not_null,where:email IS NOT NULL"`
+	FirstName   string
+	LastName    string
+	Password    string
+	BirthDate   time.Time
+	IsAdmin     bool          `gorm:"default:false"`
+	Wallet      *Wallet       `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Restaurants []*Restaurant `gorm:"many2many:user_restaurants;constraint:OnDelete:CASCADE;"` // Many-to-many relationship with roles
 }
