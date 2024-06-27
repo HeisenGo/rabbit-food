24b67b61b8d7abeec54f7bae13ddbd4b7fb1da46
kiarashfz <kiarashfz.fazli@gmail.com>
2024-06-27 05:05:35 +0330

    feat: CreateMenuForRestaurant

--- server/services/restaurant_service.go
+++ server/services/restaurant_service.go
@@ -2,23 +2,50 @@ package services
 
 import (
 	"context"
+	"server/internal/models/restaurant/menu"
 	"server/internal/models/restaurant/restaurant"
 )
 
 type RestaurantService struct {
-	restauarntOps *restaurant.Ops
+	restaurantOps *restaurant.Ops
+	menuOps       *menu.Ops
 }
 
-func NewRestaurantService(restauarntOps *restaurant.Ops) *RestaurantService {
+func NewRestaurantService(restaurantOps *restaurant.Ops, menuOps *menu.Ops) *RestaurantService {
 	return &RestaurantService{
-		restauarntOps: restauarntOps,
+		restaurantOps: restaurantOps,
+		menuOps:       menuOps,
 	}
 }
 
-func (s *RestaurantService) CreateResturantForOwner(ctx context.Context, restaurant *restaurant.Restaurant) (*restaurant.Restaurant, error) {
-	createdWallet, err := s.restauarntOps.Create(ctx, restaurant)
+func (s *RestaurantService) CreateRestaurantForOwner(ctx context.Context, restaurant *restaurant.Restaurant) (*restaurant.Restaurant, error) {
+	createdRestaurant, err := s.restaurantOps.Create(ctx, restaurant)
 	if err != nil {
 		return nil, err
 	}
-	return createdWallet, nil
+	return createdRestaurant, nil
+}
+
+func (s *RestaurantService) CreateMenuForRestaurant(ctx context.Context, menu *menu.Menu) (*menu.Menu, error) {
+	createdMenu, err := s.menuOps.Create(ctx, menu)
+	if err != nil {
+		return nil, err
+	}
+	return createdMenu, nil
+}
+
+func (s *RestaurantService) GetAllRestaurantMenus(ctx context.Context, restaurant *restaurant.Restaurant) ([]*menu.Menu, error) {
+	fetchedMenus, err := s.menuOps.GetAllRestaurantMenus(ctx, restaurant)
+	if err != nil {
+		return nil, err
+	}
+	return fetchedMenus, nil
+}
+
+func (s *RestaurantService) AddMenuItemToMenu(ctx context.Context, menuItem *menu.MenuItem) (*menu.MenuItem, error) {
+	addedMenuItem, err := s.menuOps.AddMenuItemToMenu(ctx, menuItem)
+	if err != nil {
+		return nil, err
+	}
+	return addedMenuItem, nil
 }
