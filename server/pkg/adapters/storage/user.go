0cb74c78e406a89346830a915836163ddaf5f8ff
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2024-06-30 05:06:07 +0330

    feat(storage): Implement user repository with error handling

--- server/pkg/adapters/storage/user.go
+++ server/pkg/adapters/storage/user.go
@@ -3,11 +3,14 @@ package storage
 import (
 	"context"
 	"errors"
-	"gorm.io/gorm"
+	"strings"
+
 	"server/internal/errors/users"
 	"server/internal/models/user"
 	"server/pkg/adapters/storage/entities"
 	"server/pkg/adapters/storage/mappers"
+
+	"gorm.io/gorm"
 )
 
 type userRepo struct {
@@ -18,7 +21,7 @@ func (r *userRepo) Create(ctx context.Context, user *user.User) (*user.User, err
 	newUser := mappers.UserDomainToEntity(user)
 	err := r.db.Create(&newUser).Error
 	if err != nil {
-		if errors.Is(err, gorm.ErrDuplicatedKey) {
+		if strings.Contains(err.Error(), "duplicate key") {
 			return nil, users.ErrUserExists
 		}
 		return nil, err
@@ -29,7 +32,7 @@ func (r *userRepo) Create(ctx context.Context, user *user.User) (*user.User, err
 
 func (r *userRepo) GetByPhone(ctx context.Context, phone string) (*user.User, error) {
 	var userEntity entities.User
-	err := r.db.WithContext(ctx).Model(&entities.User{}).Where("Phone = ?", phone).First(&userEntity).Error
+	err := r.db.WithContext(ctx).Model(&entities.User{}).Where("phone = ?", phone).First(&userEntity).Error
 	if err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			return nil, users.ErrUserNotFound
@@ -41,7 +44,7 @@ func (r *userRepo) GetByPhone(ctx context.Context, phone string) (*user.User, er
 
 func (r *userRepo) GetByEmail(ctx context.Context, email string) (*user.User, error) {
 	var userEntity entities.User
-	err := r.db.WithContext(ctx).Model(&entities.User{}).Where("Email = ?", email).First(&userEntity).Error
+	err := r.db.WithContext(ctx).Model(&entities.User{}).Where("email = ?", email).First(&userEntity).Error
 	if err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			return nil, users.ErrUserNotFound
@@ -49,6 +52,28 @@ func (r *userRepo) GetByEmail(ctx context.Context, email string) (*user.User, er
 		return nil, err
 	}
 	return mappers.UserEntityToDomain(&userEntity), nil
+}
+
+func (r *userRepo) GetByID(ctx context.Context, id uint) (*user.User, error) {
+	var userEntity entities.User
+	err := r.db.WithContext(ctx).Model(&entities.User{}).Where("id = ?", id).First(&userEntity).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, users.ErrUserNotFound
+		}
+		return nil, err
+	}
+	return mappers.UserEntityToDomain(&userEntity), nil
+}
+
+func (r *userRepo) Update(ctx context.Context, user *user.User) (*user.User, error) {
+	existingUser := mappers.UserDomainToEntity(user)
+	err := r.db.Save(&existingUser).Error
+	if err != nil {
+		return nil, err
+	}
+	updatedUser := mappers.UserEntityToDomain(existingUser)
+	return updatedUser, nil
 }
 
 func NewUserRepo(db *gorm.DB) user.Repo {
